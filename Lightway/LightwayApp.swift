027cd8c6cec0853b96be3a70a487760eba641343
alex <simedrea.alex@gmail.com>
2025-07-13 16:11:37 +0300

    Initial Commit

---
+++ Lightway/LightwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightwayApp.swift
+//  Lightway
+//
+//  Created by Alexandru Simedrea on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LightwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
